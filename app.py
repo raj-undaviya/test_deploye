f7decd6c1aa306559fd3a44fb58982281f69af11
Raj Undaviya <undaviyaraj2000@gmail.com>
2024-09-10 01:08:40 +0530

    Changes on port

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.secret_key = "your_secret_key"
+app.config['SESSION_COOKIE_EXPIRES'] = None
+
 @app.route('/')
 def hello():
     return "Hello, World! This is my Flask app running on Python 3.12."
