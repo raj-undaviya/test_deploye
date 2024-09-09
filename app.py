10f95bba9647499d028d1cd6160fc9b0d3e24e2b
Raj Undaviya <undaviyaraj2000@gmail.com>
2024-09-10 01:06:36 +0530

    Changes on port

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello():
     return "Hello, World! This is my Flask app running on Python 3.12."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
